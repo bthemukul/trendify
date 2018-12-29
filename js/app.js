7713276df16d63d58d07726fb390d2f79ba2e887
Mukul Bisen <bthemukul@users.noreply.github.com>
2018-12-29 20:30:23 +0530

    Added a InnerHTML element for addition of products in the Shopping Cart.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //Variables
-const tshirts = document.querySelector('#tshirt-list')
-
+const tshirts = document.querySelector('#tshirt-list'),
+        shoppingCartContent = document.querySelector('cart-content tbody');
 
 
 
@@ -29,5 +29,34 @@ function buyTshirt(e){
 }
 //Reads the HTML Info of the selected Tshirt
 function getTshirtInfo(tshirt){
-        console.log(tshirt);
+
+    const tshirtInfo = {
+        image: tshirt.querySelector('img').src,
+        title: tshirt.querySelector('h4').textContent,
+        price: tshirt.querySelector('.price span').textContent,
+        id: tshirt.querySelector('a').getAttribute('data-id')
+    }
+    addIntoCart(tshirtInfo);
+}
+
+
+//Function Displaying the products in Cart
+function addIntoCart(tshirt){
+    // creat a tr
+    const row = document.createElement('tr');
+    // Build the template
+    row.innerHTML=`
+    <tr>
+            <td>
+                <img src="${tshirt.image}">
+            </td>
+            <td> ${tshirt.title} </td>
+            <td> ${tshirt.price} </td>
+            <td>
+                <a href="#" class ='remove' data-id = "${tshirt.id}"> X</a>
+            </td>
+
+    </tr>
+    `
+
 }
